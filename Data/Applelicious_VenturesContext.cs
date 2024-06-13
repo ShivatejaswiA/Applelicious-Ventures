5d3a2d07d68add03a4231f58a49a8bb6702b8c27
user <user@DESKTOP-8923KSC>
2024-06-13 15:59:21 -0400

    Add project files.

---
+++ Data/Applelicious_VenturesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Applelicious_Ventures.Models;
+
+namespace Applelicious_Ventures.Data
+{
+    public class Applelicious_VenturesContext : DbContext
+    {
+        public Applelicious_VenturesContext (DbContextOptions<Applelicious_VenturesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Applelicious_Ventures.Models.Apple> Apple { get; set; } = default!;
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Applelicious_Ventures.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Linq;
+
+
+namespace Applelicious_Ventures.Models;
+
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Applelicious_VenturesContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<Applelicious_VenturesContext>>()))
+            {
+                // Look for any movies.
+                if (context.Apple.Any())
+                {
+                    return;   // DB has been seeded
+                }
+            context.Apple.AddRange(
+                new Apple
+                {
+                    Origin = "India",
+                    ImportDate = DateTime.Parse("-2-12"),
+                    Color = "Romantic Comedy",
+                    Price = 7.99M,
+                   
+
+
+                },
+                new Apple
+                {
+                    Origin = "India",
+                    ImportDate = DateTime.Parse("1989-2-12"),
+                    Color = "Romantic Comedy",
+                    Price = 7.99M
+                },
+                new Apple
+                {
+                    Origin = "India",
+                    ImportDate = DateTime.Parse("1989-2-12"),
+                    Color = "Romantic Comedy",
+                    Price = 7.99M
+                },
+                new Apple
+                {
+                    Origin = "India",
+                    ImportDate = DateTime.Parse("1989-2-12"),
+                    Color = "Romantic Comedy",
+                    Price = 7.99M
+                }
+            );
+                context.SaveChanges();
+            }
+        }
+    }
