09f75a34c274f2b7c253251d8d2b1c58a9003da7
Dhariya Bhardwaj <W0852423@myscc.ca>
2024-06-13 22:59:27 -0400

    Update

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 using System;
+using System.Drawing;
 using System.Linq;
 
 
@@ -26,33 +27,104 @@ namespace Applelicious_Ventures.Models;
                 {
                     Origin = "India",
                     ImportDate = DateTime.Parse("2024-2-12"),
-                    Color = "Romantic Comedy",
+                    Color = "Romantic Red",
                     Price = 7.99M,
                    
 
 
                 },
+                 new Apple
+                 {
+                     Origin = "Iran",
+                     ImportDate = DateTime.Parse("2024-2-01"),
+                     Color = "Brown",
+                     Price = 3.99M,
+
+
+
+                 },
+                  new Apple
+                  {
+                      Origin = "Thailand",
+                      ImportDate = DateTime.Parse("2024-2-03"),
+                      Color = "Brown Light",
+                      Price = 6.99M,
+
+
+
+                  },
+                   new Apple
+                   {
+                       Origin = "NZ",
+                       ImportDate = DateTime.Parse("2024-2-04"),
+                       Color = "Red Purple",
+                       Price = 2.99M,
+
+
+
+                   },
+                    new Apple
+                    {
+                        Origin = "Ireland",
+                        ImportDate = DateTime.Parse("2024-2-03"),
+                        Color = "Fetish Pink",
+                        Price = 15.99M,
+
+
+
+                    },
+                 new Apple
+                 {
+                Origin = "UK",
+                    ImportDate = DateTime.Parse("2024-2-11"),
+                    Color = "Red",
+                    Price = 6.99M,
+                   
+
+
+                },
+                  new Apple
+                  {
+                      Origin = "Switzerland",
+                      ImportDate = DateTime.Parse("2024-2-10"),
+                      Color = "Fetish Red",
+                      Price = 5.99M,
+
+
+
+                  },
+                   new Apple
+                   {
+                       Origin = "Estonia",
+                       ImportDate = DateTime.Parse("2024-2-09"),
+                       Color = "Childish Red",
+                       Price = 8.99M,
+
+
+
+                   },
                 new Apple
                 {
-                    Origin = "India",
-                    ImportDate = DateTime.Parse("1989-2-12"),
-                    Color = "Romantic Comedy",
-                    Price = 7.99M
+                    Origin = "Lithuania",
+                    ImportDate = DateTime.Parse("1989-2-08"),
+                    Color = "Green Apple",
+                    Price = 9.99M
                 },
                 new Apple
                 {
-                    Origin = "India",
-                    ImportDate = DateTime.Parse("1989-2-12"),
-                    Color = "Romantic Comedy",
-                    Price = 7.99M
+                    Origin = "Nepal",
+                    ImportDate = DateTime.Parse("2024-2-07"),
+                    Color = "Light Green",
+                    Price = 8.99M
                 },
                 new Apple
                 {
-                    Origin = "India",
-                    ImportDate = DateTime.Parse("1989-2-12"),
-                    Color = "Romantic Comedy",
-                    Price = 7.99M
+                    Origin = "Latvia",
+                    ImportDate = DateTime.Parse("2024-2-06"),
+                    Color = "Romantic Pink",
+                    Price = 11.99M
                 }
+
             );
                 context.SaveChanges();
             }
